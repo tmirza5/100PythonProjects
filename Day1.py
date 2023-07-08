d0c85c9b5d7f824eaf2a659478194d48dcf1cb92
Muhammad Taqi Mirza <tmirza5@outlook.com>
2023-07-08 22:11:51 +0100

    Create and complete Day1.py
    Day1 created and fully completed. All comments and lines of code are final.

---
+++ Day1.py
@@ -0,0 +1,11 @@
+### This is the final project for Day 1. Name: Band Name Generator
+##Program Start
+#Program Greeting
+print("Welcome to the Band Name Generator!")
+
+#Input for Childhood City name and Pet name
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your pet?\n")
+
+#Output band name
+print(f"Your band name could be {city} {pet}")
