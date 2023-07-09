858db73f936a809a83e76c2de7ccf66e20d26319
Muhammad Taqi Mirza <tmirza5@outlook.com>
2023-07-09 21:15:45 +0100

    Update Day7.py
    Day7.py created and fully completed. All comments and lines of code are for Challenge 2/5 only. Further commits will be made.

--- Day7.py
+++ Day7.py
@@ -1,4 +1,4 @@
-### This is the first project for Day 7. Name: Hangman
+### This is the final project for Day 7. Name: Hangman
 ##Program Start
 #Import required modules
 import random
@@ -9,13 +9,28 @@ word_bank = ['ardvark', 'baboon', 'camel']
 #Choose random word
 chosen_word = random.choice(word_bank)
 
+#Print the chosen work (for testing)
+print(f"Chosen word is {chosen_word}")
+
+#Convert the word into a list
+check = list(chosen_word)
+
+#Create the display skeleton
+display = []
+for letter in check:
+    display.append("_")
+print(display)
+
 #Ask user to guess a letter
-guess = input("Guess a letter: ")
+guess = input("Guess a letter: ").lower()
 
 #Check if that letter is in the word
-check = list(chosen_word)
+iteration = 0
 for letter in check:
+    iteration += 1
     if letter == guess:
-        print("Right")
+        display[iteration-1] = letter
     else:
-        print("Wrong")
+        continue
+
+print(display)
